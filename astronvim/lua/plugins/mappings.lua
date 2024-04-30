5b782224abac28bf18a5bcd544adb97c4ceef879
Martin David Valentino <martin.valentino@cultureamp.com>
2024-04-30 11:20:56 +1000

    feat: add astronvim config

---
+++ astronvim/lua/plugins/mappings.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "AstroNvim/astrocore",
+    ---@type AstroCoreOpts
+    opts = {
+      mappings = {
+        -- first key is the mode
+        n = {
+          -- second key is the lefthand side of the map
+          -- mappings seen under group name "Buffer"
+          ["q"] = { ":quit<cr>", desc = "Quit buffer" },
+          -- tables with the `name` key will be registered with which-key if it's installed
+          -- quick save
+          -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+        },
+        t = {
+          -- setting a mapping to false will disable it
+          -- ["<esc>"] = false,
+        },
+      },
+    },
+  },
+}
