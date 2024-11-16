97e41b2e3acff9022334c36e8254c37ec25b84bf
Martin Valentino <mvalentino@atlassian.com>
2024-11-16 12:47:39 +1100

    fix: refine the Gitlinker setup to works with bitbucket and stash

--- astronvim/lua/plugins/gitlinker.lua
+++ astronvim/lua/plugins/gitlinker.lua
@@ -1,8 +1,8 @@
 local function stash_matcher(url_data)
   local url = "https://stash.atlassian.com/projects/"
-  url = url .. url_data.user .. "/repos/" .. url_data.repo:gsub("%.git", "")
+  url = url .. "atlassian/repos/" .. url_data.repo:gsub("%.git", "")
   url = url .. "/browse/" .. url_data.file
-  url = url .. "?at=" .. url_data.rev
+  -- url = url .. "?at=master" .. url_data.rev
   if url_data.lend then
     url = url .. "#" .. url_data.lstart .. "-" .. url_data.lend
   else
